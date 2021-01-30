4db5c9168853bbcdd0473935beacdb68d497c529
Marek <fx@marksoft.com.pl>
2021-01-30 10:17:26 +0100

    feat(app): AIO

---
+++ src/app/component/contact/contact.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit, OnDestroy {
+
+  personName: string;
+
+  constructor() {
+    console.log('constructor()');
+  }
+
+  ngOnInit(): void {
+    // incicjalizacja tutaj
+    this.personName = 'Mateusz';
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy()');
+  }
+
+}
